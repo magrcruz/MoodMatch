1e65fd7623674271cdb4d511cc83b5baec999676
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-05-20 20:38:12 -0500

    placeholers sign up

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Usuario', 'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Contraseña', 'class': 'form-control'}))
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'placeholder': 'Nombre',"class":"form-control"}))
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'placeholder': 'Nombre de usuario',"class":"form-control"})
+    )
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Correo',"class":"form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña',"class":"form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirmar Contraseña',"class":"form-control"}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.views import LoginView
-
-from .forms import CustomLoginForm
+from .forms import CustomLoginForm, SignUpForm
 
 class CustomLoginView(LoginView):
     form_class = CustomLoginForm
     template_name = 'registration/login.html'  # Especifica el nombre de tu plantilla personalizada de inicio de sesión
     success_url = '/'  # Especifica la URL a la que se redirigirá después del inicio de sesión exitoso
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"

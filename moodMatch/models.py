344ec49b9acc043195f9e3d64622363ca9ec7dfd
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-05-21 18:43:18 -0500

    Backend genre preferences

--- moodMatch/models.py
+++ moodMatch/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 from .modelsRecomendation import *
 # Create your models here.
+
+class UserPreferences(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    genres_preferences = models.ManyToManyField(Genre)
--- moodMatch/views.py
+++ moodMatch/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
@@ -43,16 +43,61 @@ def choose_content(request):
 
 @login_required
 def genre_preferences(request):
+    s_genres = Genre.objects.filter(type='song')
+    f_genres = Genre.objects.filter(type='film')
+
     if request.method == 'POST':
         selected_song_genres = request.POST.getlist('song_genres') 
         selected_film_genres = request.POST.getlist('film_genres')
+        
         # Guardar preferencias
+        user_preferences, created = UserPreferences.objects.get_or_create(user=request.user)
 
-        return HttpResponse(selected_song_genres + selected_film_genres)
-    
-    song_genres = Genre.objects.filter(type='song')
-    film_genres = Genre.objects.filter(type='film')
-    
+        # Obtener géneros seleccionados previamente
+        selected_songs = user_preferences.genres_preferences.filter(type='song')
+        selected_films = user_preferences.genres_preferences.filter(type='film')
+
+        # Agregar géneros seleccionados actualmente
+        for genre_id in selected_song_genres:
+            genre = Genre.objects.get(id=genre_id)
+            if genre not in selected_songs:
+                user_preferences.genres_preferences.add(genre)
+        
+        for genre_id in selected_film_genres:
+            genre = Genre.objects.get(id=genre_id)
+            if genre not in selected_films:
+                user_preferences.genres_preferences.add(genre)
+
+        # Eliminar géneros deseleccionados
+        for genre in selected_songs:
+            if str(genre.id) not in selected_song_genres:
+                user_preferences.genres_preferences.remove(genre)
+        
+        for genre in selected_films:
+            if str(genre.id) not in selected_film_genres:
+                user_preferences.genres_preferences.remove(genre)
+
+        return redirect(reverse("moodMatch:choose_content"))
+
+    try:
+        user_preferences = UserPreferences.objects.get(user=request.user)
+    except UserPreferences.DoesNotExist:
+        # Si no existe un objeto UserPreferences, crear uno nuevo
+        user_preferences = UserPreferences.objects.create(user=request.user)
+
+    selected_songs = user_preferences.genres_preferences.filter(type='song')
+    selected_films = user_preferences.genres_preferences.filter(type='film')
+
+    # Agregar campo adicional 'isSelected'
+    song_genres = [
+        {'genre': genre, 'isSelected': genre in selected_songs}
+        for genre in s_genres
+    ]
+    film_genres = [
+        {'genre': genre, 'isSelected': genre in selected_films}
+        for genre in f_genres
+    ]
+
     return render(request, 'moodMatch/genre_preferences.html', {'song_genres': song_genres, 'film_genres': film_genres})
 
 def show_info(request):

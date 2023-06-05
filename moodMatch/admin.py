9ec927aae9ed2e5efa2688223c55e911e1c74cad
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-06-05 00:37:16 -0500

    Backend Recommendations Left

--- moodMatch/admin.py
+++ moodMatch/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Genre, Content, Emotion
+from .models import Genre, Content, Emotion, UserRecomendations
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Content)
 admin.site.register(Emotion)
+admin.site.register(UserRecomendations)
--- moodMatch/modelInterface.py
+++ moodMatch/modelInterface.py
@@ -1,22 +1,36 @@
-from .modelsRecomendation import Content, Emotion
-from .models import UserPreferences
+from .modelsRecomendation import Content, Emotion, Genre
+from .models import UserPreferences, UserRecomendations
 import random
+
 
 def generate_recommendation(request,num_words, type, emotion):
     
+    # Verificar que tiene intentos disponibles
+    user_recomendations, created = UserRecomendations.objects.get_or_create(user=request.user, defaults={'totalRecomendations': 0, 'recomendationsleft': 0})
+    
+    if user_recomendations.can_recharge():
+        user_recomendations.recharge()
+    
+    if not user_recomendations.has_recommendations():
+        return []
+
     emotion = Emotion.objects.get(name=emotion)
 
     # Obtener las preferencias de género del usuario
-    user_preferences = UserPreferences.objects.get(user=request.user)
+    user_preferences, created = UserPreferences.objects.get_or_create(user=request.user)
 
     # Obtener los géneros preferidos del usuario
     genres_preferred = user_preferences.genres_preferences.all()
-
+    if not genres_preferred:
+        # Si el usuario no tiene preferencias, asigna todos los géneros disponibles
+        genres_preferred = Genre.objects.all()
+    
     # Consulta la base de datos para obtener las instancias que cumplan con las características
     recommendations = Content.objects.filter(
         type=type,
         emotion = emotion,
         genre__in=genres_preferred
     ).order_by('?')
-
+    if len(list(recommendations)):
+        user_recomendations.use_recomendation()
     return list(recommendations)[:num_words]

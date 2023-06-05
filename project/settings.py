d5ca557e02fdd8886c4f651a63f17ba3296d4669
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-06-04 22:22:44 -0500

    Frontend Icons

--- project/settings.py
+++ project/settings.py
@@ -137,3 +137,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_CHARSET = 'utf-8'
